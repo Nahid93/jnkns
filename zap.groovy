ed0669e3bdaa7849009ec08dedc8614b1a0244eb
zmamnah <nahid.mammadli@ericsson.com>
2022-08-07 14:31:17 +0200

    add paramater URL

--- zap.groovy
+++ zap.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('Build') {
             steps {
-                sh 'ssh root@192.168.0.51  "zap.sh -daemon -quickurl https://www.qafqazinfo.az/  -quickout /opt/OWSPZP/nahid.rp"'
+                sh 'ssh root@192.168.0.51  "zap.sh -daemon -quickurl ${URL} "'
             }
         }
     }
