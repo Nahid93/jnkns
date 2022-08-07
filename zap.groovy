cb6b612ab0d6a2271bf9083bfda44efd63a61695
Nahid93 <55858554+Nahid93@users.noreply.github.com>
2022-08-07 13:25:32 +0200

    Update zap.groovy

--- zap.groovy
+++ zap.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('Build') {
             steps {
-                sh 'ssh root @192.168.0.51 -pw "Xelisa#29" -m "zap.sh -daemon -quickurl https://www.qafqazinfo.az/  -quickout /opt/OWSPZP/nahid.rp"'
+                sh 'ssh root@192.168.0.51 -pw "Xelisa#29" -m "zap.sh -daemon -quickurl https://www.qafqazinfo.az/  -quickout /opt/OWSPZP/nahid.rp"'
             }
         }
     }
